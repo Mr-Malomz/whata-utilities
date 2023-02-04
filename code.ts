f1c083e62cfee5708df3beadb82a2bf259b9d44b
demola malomo <malomo.alaba@yahoo.com>
2023-02-04 01:10:21 +0100

    add topLeft logic

--- code.ts
+++ code.ts
@@ -1,3 +1,30 @@
 figma.showUI(__html__);
 
 figma.ui.resize(400, 300);
+
+figma.ui.onmessage = (pluginMessage) => {
+	//check if user select design element
+	if (
+		figma.currentPage.selection.length == 0 ||
+		figma.currentPage.selection == undefined
+	) {
+		figma.closePlugin();
+	} else {
+		//get seletected item and apply radius
+		const selected = figma.currentPage.selection[0] as ComponentNode;
+		if (
+			selected &&
+			(selected.type == 'COMPONENT' ||
+				selected.type == 'COMPONENT_SET' ||
+				selected.type == 'FRAME' ||
+				selected.type == 'INSTANCE' ||
+				selected.type == 'RECTANGLE')
+		) {
+			selected.topLeftRadius = 76;
+		} else {
+			figma.closePlugin();
+		}
+	}
+
+	// figma.closePlugin();
+};
